cba2e7e5d4e953fbeeb87df26446a535d4feae86
Denis1988-hub <cryptomining1988@gmail.com>
2022-11-13 11:14:05 +0200

    create layout

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <div
+      id="layout"
+      className="layout relative flex flex-col gap-y-[50px] overflow-hidden"
+    >
+      <header></header>
+      <main className="container">
+        <Outlet />
+      </main>
+      <footer></footer>
+    </div>
+  );
+};
+
+export default Layout;
